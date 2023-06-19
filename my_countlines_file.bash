155e3aa14f88fa57aa5e071b60bef0725a5c2850
renegc3pillar <rene.gonzalez@3pillarglobal.com>
2023-06-19 12:05:00 -0700

    Added some functions

--- my_countlines_file.bash
+++ my_countlines_file.bash
@@ -1,43 +1,52 @@
-while getopts ":o:m:" option; do
-case ${option} in
-o)
-echo "Looking for files where the owner is: "${OPTARG^^}
-search_dir=$(pwd)
+print_result() {
+	path=$3
+	name=$4
 
-for entry in "$search_dir"/*
-do
+	if [ $1 == $2 ]; then
+	  echo "File: $name, Lines: $(awk ' END { print NR }' $path) $name"
+	fi
+}
 
-   file_path="$entry"
-   file_name="$(basename $file_path)"
-   file_owner=$(stat -c "%U" "$entry")
+check_options() {
+	while getopts ":o:m:" option; do
+	case ${option} in
+	o)
+		echo "Looking for files where the owner is: "${OPTARG^^}
+		search_dir=$(pwd)
 
-   if [ ${OPTARG^^} == ${file_owner^^} ]; then
-      echo "File: $file_name, Lines: $(awk ' END { print NR }' $file_path ) $file_name"
-   fi
+		for entry in "$search_dir"/*
+		do
 
-done
-;;
-m)
-echo "Looking for files where the month is: "${OPTARG^^}
-search_dir=$(pwd)
+		   file_path="$entry"
+		   file_name="$(basename $file_path)"
+		   file_owner=$(stat -c "%U" "$entry")
 
-for entry in "$search_dir"/*
-do
-   file_path="$entry"
-   file_name="$(basename $file_path)"
-   file_date=$(stat -c "%w" "$entry")
-   format_date="$(echo $file_date| cut -d'-' -f 1)-$(echo $file_date| cut -d'-' -f 2)-01"
-   month=$(date -d $format_date '+%b')
-   
-   
-   if [ ${OPTARG^^} == ${month^^} ]; then
-	echo "File: $file_name, Lines: $(awk ' END { print NR }' $file_path) $file_name"
-   fi
-done
+		   print_result ${OPTARG^^} ${file_owner^^} $file_path $file_name
 
-;;
-:)
-echo "You have entered an invalid option. The valid options are [o],[m]"
-;;
-esac
-done
+		done
+	;;
+	m)
+		echo "Looking for files where the month is: "${OPTARG^^}
+		search_dir=$(pwd)
+
+		for entry in "$search_dir"/*
+		do
+		   file_path="$entry"
+		   file_name="$(basename $file_path)"
+		   file_date=$(stat -c "%w" "$entry")
+		   format_date="$(echo $file_date| cut -d'-' -f 1)-$(echo $file_date| cut -d'-' -f 2)-01"
+		   month=$(date -d $format_date '+%b')
+
+		   print_result ${OPTARG^^} ${month^^} $file_path $file_name      		  
+
+		done
+
+	;;
+	:)
+	echo "You have entered an invalid option. The valid options are [o],[m]"
+	;;
+	esac
+	done
+}
+
+check_options $1 $2
